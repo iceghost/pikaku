4c9289236ec5004675b7b8bc4d9e9b33f9d25577
iceghost <khangemail22@gmail.com>
2022-03-23 11:00:05 +0700

    Relax typing

--- pipes_v2/board.py
+++ pipes_v2/board.py
@@ -1,8 +1,11 @@
 from enum import Enum, IntEnum
 from typing import Optional, Tuple
 
+import numpy
+import numpy.typing
 
-class PipeType(Enum):
+
+class PipeType(IntEnum):
     End = 0
     Corner = 1
     Long = 2
@@ -10,32 +13,32 @@ class PipeType(Enum):
 
 
 class Joint(IntEnum):
-    Unknown = 0
-    Connected = 1
-    Unconnected = 2
+    UNKNOWN = -1
+    UNCONNECTED = 0
+    CONNECTED = 1
 
 
 class Joints:
     def __init__(self, height: int, width: int):
         # v for vertical, h for horizontal
-        self.v_joints = [[Joint.Unknown] * (width + 1) for _ in range(0, height)]
-        self.h_joints = [[Joint.Unknown] * (width) for _ in range(0, height + 1)]
+        self.v_joints = numpy.full((height, width + 1), Joint.UNKNOWN, numpy.int8)
+        self.h_joints = numpy.full((height + 1, width), Joint.UNKNOWN, numpy.int8)
 
         # initialize border for non-wrap pipe game
-        for x in range(0, width + 1):
-            self.v_joints[0][x] = Joint.Unconnected
-            self.v_joints[height-1][x] = Joint.Unconnected
+        self.v_joints[:, 0] = Joint.UNCONNECTED
+        self.v_joints[:, -1] = Joint.UNCONNECTED
+        self.h_joints[0, :] = Joint.UNCONNECTED
+        self.h_joints[-1, :] = Joint.UNCONNECTED
 
-        for y in range(0, height + 1):
-            self.h_joints[y][0] = Joint.Unconnected
-            self.h_joints[y][width-1] = Joint.Unconnected
+        self.unknowns = numpy.count_nonzero(self.v_joints) + numpy.count_nonzero(
+            self.h_joints
+        )
 
-
-    def at(self, x: int, y: int) -> Tuple[Joint, Joint, Joint, Joint]:
-        top = self.h_joints[y][x]
-        bottom = self.h_joints[y + 1][x]
-        left = self.v_joints[y][x]
-        right = self.v_joints[y][x + 1]
+    def at(self, x: int, y: int) -> Tuple[int, int, int, int]:
+        top = self.h_joints[y, x]
+        bottom = self.h_joints[y + 1, x]
+        left = self.v_joints[y, x]
+        right = self.v_joints[y, x + 1]
         return top, right, bottom, left
 
     def set(
@@ -48,10 +51,10 @@ class Joints:
         left: Optional[Joint] = None,
     ):
         if top is not None:
-            self.h_joints[y][x] = top
+            self.h_joints[y, x] = top
         if bottom is not None:
-            self.h_joints[y + 1][x] = bottom
+            self.h_joints[y + 1, x] = bottom
         if left is not None:
-            self.v_joints[y][x] = left
+            self.v_joints[y, x] = left
         if right is not None:
-            self.v_joints[y][x + 1] = right
+            self.v_joints[y, x + 1] = right
