fe66b1d085c70daa848ca49255fa2f9ff16986b8
iceghost <khangemail22@gmail.com>
2022-03-30 13:02:47 +0700

    simplify a bit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import logging
-from pipes_v2.agent import blind_search, heuristic_search
+from pipes_v2.agent import blind_search, heuristic_search, fast_search
 from pipes_v2.board import Board
 from pipes_v2.utils.iterator import exhaust
 from pipes_v2.utils.generate import generate_board
@@ -7,28 +7,22 @@ from pipes_v2.utils.image_proc import download_board
 import timeit
 
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 
-def blind_solve(board: Board):
-    state, _ = exhaust(blind_search(board))
+def solve(board: Board, search_algorithm):
+    state = search_algorithm(board)
     if state is None:
         raise
-    print(state.joints)
-
-
-def heuristic_solve(board: Board):
-    state = exhaust(heuristic_search(board))
-    if state is None:
-        raise
-    print(state.joints)
+    logging.info("Result:\n{}".format(state.joints))
 
 
 if __name__ == "__main__":
     board = download_board(
-        "https://www.puzzle-pipes.com/screenshots/69becb4d45af49d9a092e1a9d1e5ce0b62411aa22a250.png",
-        40,
-        40,
+        "https://www.puzzle-pipes.com/screenshots/4154dfb2feb62e923c64ce28ae86d2236243dae951f47.png",
+        20,
+        20,
     )
-    print(timeit.timeit("blind_solve(board)", number=1, globals=locals()))
-    print(timeit.timeit("heuristic_solve(board)", number=1, globals=locals()))
+    # print(timeit.timeit("solve(board, heuristic_search)", number=1, globals=locals()))
+    print(timeit.timeit("solve(board, blind_search)", number=1, globals=locals()))
+    # print(timeit.timeit("solve(board, fast_search)", number=1, globals=locals()))
--- pipes_v2/agent.py
+++ pipes_v2/agent.py
@@ -1,42 +1,8 @@
-from dataclasses import dataclass, field
-from enum import Enum
-from queue import LifoQueue, PriorityQueue
-from typing import Optional, Tuple
+import logging
+from queue import LifoQueue
+from typing import Tuple
 from pipes_v2.board import Board
 from pipes_v2.state import Detached, Looped, Solved, State
-
-
-class Status(Enum):
-    TRY = 0
-    SOLVE = 1
-    SOLVED = 2
-    LOOPED = 3
-    DETACHED = 4
-    DEADEND = 5
-
-    def message(self, x, y):
-        if self is Status.TRY:
-            return "Thử hướng quay tại ({: >2}, {: >2})".format(x, y)
-        elif self is Status.SOLVE:
-            return "Giải các ô chỉ có một hướng xoay"
-        elif self is Status.SOLVED:
-            return "Bàn cờ đã được giải"
-        elif self is Status.LOOPED:
-            return (
-                "Phát hiện vi phạm luật vòng lặp khi duyệt tại ({: >2}, {: >2})".format(
-                    x, y
-                )
-            )
-        elif self is Status.DETACHED:
-            return (
-                "Phát hiện vi phạm luật kết nối khi duyệt tại ({: >2}, {: >2})".format(
-                    x, y
-                )
-            )
-        elif self is Status.DEADEND:
-            return "Ô ({: >2}, {: >2}) không thể xoay đi đâu nữa".format(x, y)
-        else:
-            return "... what"
 
 
 def blind_search(board: Board):
@@ -44,63 +10,94 @@ def blind_search(board: Board):
     states.put((State(board.HEIGHT, board.WIDTH), 0, 0))
     while not states.empty():
         state, x, y = states.get()
-        yield state, (x, y, Status.TRY)
-        # state.solve(board)
-        # yield state, (x, y, Status.SOLVE)
+        state.solve(board)
+
+        logging.debug("joints: \n{}".format(state.joints))
         try:
             state.simplify_at(x, y)
+            logging.debug("iso-joints: \n{}".format(state.iso_joints))
         except Solved:
-            yield state, (None, None, Status.SOLVED)
-            return
-        except Looped:
-            yield state, (x, y, Status.LOOPED)
-            continue
-        except Detached:
-            yield state, (x, y, Status.DETACHED)
+            return state
+        except (Looped, Detached):
             continue
 
-        x, y = next(state.next_pipes())
-        if sum(1 for _ in state.next_configs(x, y, board)) == 0:
-            yield state, (x, y, Status.DEADEND)
-        else:
-            for state in state.next_states(x, y, board):
-                states.put((state, x, y))
-    return
-
-
-@dataclass(order=True)
-class HeuristicItem:
-    score: int
-    data: Tuple[State, int, int] = field(compare=False)
+        next_x, next_y = next(state.next_pipes())
+        for next_state in state.next_states(next_x, next_y, board):
+            states.put((next_state, next_x, next_y))
 
 
 def heuristic_search(board: Board):
-    queue: PriorityQueue[HeuristicItem] = PriorityQueue()
-    queue.put(HeuristicItem(0, (State(board.HEIGHT, board.WIDTH), 0, 0)))
+    queue: LifoQueue[Tuple[State, int, int]] = LifoQueue()
+    queue.put((State(board.HEIGHT, board.WIDTH), 0, 0))
     while not queue.empty():
-        state, x, y = queue.get().data
-        yield state
-        state.solve(board)
-        yield state
+        state, x, y = queue.get()
+
         try:
             state.simplify_at(x, y)
         except Solved:
-            return
+            return state
         except (Looped, Detached):
             continue
-        # finally:
-        #     print(state.iso_joints)
-        x, y, score = None, None, 2
-        while True:
-            try:
-                x, y = next(
-                    (x, y)
-                    for x, y in state.next_pipes()
-                    if sum(1 for _ in state.next_configs(x, y, board)) == score
-                )
-                break
-            except StopIteration:
-                score = score + 1
 
+        x, y = min(
+            state.next_pipes(),
+            key=lambda tup: sum(1 for _ in state.next_configs(tup[0], tup[1], board)),
+        )
         for next_state in state.next_states(x, y, board):
-            queue.put(HeuristicItem(score, (next_state, x, y)))
+            queue.put((next_state, x, y))
+
+
+def fast_search(board: Board):
+    queue: LifoQueue[Tuple[State, int, int]] = LifoQueue()
+    queue.put((State(board.HEIGHT, board.WIDTH), 0, 0))
+    while not queue.empty():
+        state, x, y = queue.get()
+        state.solve(board)
+
+        try:
+            state.simplify_at(x, y)
+        except Solved:
+            return state
+        except (Looped, Detached):
+            continue
+
+        next_x, next_y = next(state.next_pipes(y * board.WIDTH + x))
+        for next_state in state.next_states(next_x, next_y, board):
+            queue.put((next_state, next_x, next_y))
+
+
+# def verbose_blind_search(board: Board, logger: Optional[Logger] = None):
+#     logger = logger or logging.getLogger("__void")
+#     states: LifoQueue[Tuple[State, int, int]] = LifoQueue()
+#     states.put((State(board.HEIGHT, board.WIDTH), 0, 0))
+#     while not states.empty():
+#         state, x, y = states.get()
+#         logger.debug("Tried a configuration at ({: >2}, {: >2})".format(x, y))
+#         yield state
+
+#         # state.solve(board)
+#         # logger.info("Solves pipes with only one configuration")
+#         # yield state
+#         try:
+#             state.simplify_at(x, y)
+#         except Solved:
+#             logger.debug("Board has been solved")
+#             yield state
+#             return state
+#         except Looped:
+#             logger.debug("Loop detected at ({: >2}, {: >2})".format(x, y))
+#             yield state
+#             continue
+#         except Detached:
+#             logger.debug("Multiple groups detected at ({: >2}, {: >2})".format(x, y))
+#             yield state
+#             continue
+
+#         x, y = next(state.next_pipes())
+#         if sum(1 for _ in state.next_configs(x, y, board)) == 0:
+#             logger.debug("Dead-end at ({: >2}, {: >2})".format(x, y))
+#             yield state
+#             continue
+
+#         for state in state.next_states(x, y, board):
+#             states.put((state, x, y))
