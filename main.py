1400213881f9dc547afe9816e4ec201bbc48d617
iceghost <khangemail22@gmail.com>
2022-03-31 11:29:57 +0700

    Add shiny new CLI

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
+#!/usr/bin/env python3
+import argparse
 import logging
-from pipes_v2.agent import blind_search, heuristic_search, fast_search
+from pipes_v2.agent import blind_search, heuristic_search, improved_search
 from pipes_v2.board import Board
-from pipes_v2.utils.iterator import exhaust
 from pipes_v2.utils.generate import generate_board
 from pipes_v2.utils.image_proc import download_board
 import timeit
-
-
-logging.basicConfig(level=logging.DEBUG)
 
 
 def solve(board: Board, search_algorithm):
@@ -17,12 +15,49 @@ def solve(board: Board, search_algorithm):
     logging.info("Result:\n{}".format(state.joints))
 
 
-if __name__ == "__main__":
+parser = argparse.ArgumentParser(
+    description="solve pipes puzzle with selected algorithm"
+)
+parser.add_argument("width", type=int)
+parser.add_argument("height", type=int)
+parser.add_argument("-d", "--debug", action="store_true")
+parser.add_argument(
+    "-a",
+    "--algorithm",
+    choices=["blind", "heuristic", "improved"],
+    default="improved",
+)
+parser.add_argument(
+    "-s",
+    "--seed",
+    help="if there's no url then generate random board. seed can be specified for reproducibility",
+)
+parser.add_argument(
+    "-u",
+    "--url",
+    help="""download board from www.puzzle-pipes.com
+(please use dark mode and copy screenshot link from share button). this take precedence over --seed""",
+)
+args = parser.parse_args()
+
+logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO)
+
+board = None
+if args.url is not None:
     board = download_board(
-        "https://www.puzzle-pipes.com/screenshots/4154dfb2feb62e923c64ce28ae86d2236243dae951f47.png",
-        20,
-        20,
+        args.url,
+        args.height,
+        args.width,
     )
-    # print(timeit.timeit("solve(board, heuristic_search)", number=1, globals=locals()))
-    print(timeit.timeit("solve(board, blind_search)", number=1, globals=locals()))
-    # print(timeit.timeit("solve(board, fast_search)", number=1, globals=locals()))
+else:
+    board = generate_board(args.height, args.width, args.seed)
+
+logging.info(
+    "time taken: {}".format(
+        timeit.timeit(
+            "solve(board, {alg}_search)".format(alg=args.algorithm),
+            number=1,
+            globals=locals(),
+        )
+    )
+)
